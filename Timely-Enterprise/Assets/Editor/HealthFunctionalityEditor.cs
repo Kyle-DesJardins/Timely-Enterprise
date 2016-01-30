d3f01196b1a4e8d78352196171295d616e91af6c
Alex Jagim <ajagim@mail.bradley.edu>
2016-01-30 16:33:14 -0600

    XML SHTUFF

---
+++ Timely-Enterprise/Assets/Editor/HealthFunctionalityEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(HealthFunctionality))]
+public class NewBehaviourScript : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        HealthFunctionality healthFunctionalityScript = (HealthFunctionality)target;
+        DrawDefaultInspector();
+        healthFunctionalityScript.setTotalHealth(EditorGUILayout.Slider(healthFunctionalityScript.getTotalHealth(), 0, healthFunctionalityScript.getMaxHealth()));
+    }
+
+}
---
+++ Timely-Enterprise/Assets/Scripts/Misc/JournalCollection.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[System.Serializable]
+public class JournalEntry
+{
+    [XmlAttribute("date")]
+    public string date;
+
+    [XmlElement("Paragraph1")]
+    public string paragraph1;
+
+    [XmlElement("Paragraph2")]
+    public string paragraph2;
+}
+
+[XmlRoot("JournalEntries")]
+public class JournalCollection {
+
+    [XmlArray("Person")]
+    [XmlArrayItem("Entry")]
+    public List<JournalEntry> entries = new List<JournalEntry>();
+
+    public static JournalCollection Load(string path)
+    {
+        TextAsset _xml = Resources.Load<TextAsset>(path);
+
+        XmlSerializer serializer = new XmlSerializer(typeof(JournalCollection));
+
+        StringReader reader = new StringReader(_xml.text);
+
+        JournalCollection text = serializer.Deserialize(reader) as JournalCollection;
+
+        reader.Close();
+
+        return text;
+
+    }
+}
