53ec95f59f836549f3f890f6003d78f8c9994164
Kyle Des Jardins <kdesjardins@mail.bradley.edu>
2016-01-30 14:16:02 -0600

    Animations
    Added door animations and some more enemy animations.

--- Timely-Enterprise/Assets/Scripts/Character/InteractFunctionality.cs
+++ Timely-Enterprise/Assets/Scripts/Character/InteractFunctionality.cs
@@ -9,11 +9,61 @@ public class InteractFunctionality : MonoBehaviour {
     [HideInInspector]
     public string interactableTag;
 
+	private bool interacte;
+	private bool animate;
+	private Renderer render;
+	private Collider2D collider;
+	private Rigidbody2D rigidbody;
+	private Collider2D animateCollider;
+
+	private bool animating;
+	private float animTimer;
+
+	void Awake() {
+		interacte = false;
+		render = gameObject.GetComponent<Renderer>();
+		collider = gameObject.GetComponent<Collider2D>();
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+		animating = false;
+	}
+
     void Update()
     {
-        if (Input.GetKeyDown(customKeyCode))
+        if (Input.GetKeyDown(customKeyCode) && interacte == true)
         {
-            Debug.Log(transform.name + " Interact");
-        }
+            Debug.Log(transform.name + animateCollider + " Interact");
+			animating = true;
+			animateCollider.GetComponent<Animator>().SetInteger("Open", 1);
+		}
+
+		if(animating == true) {
+			animTimer += Time.deltaTime;
+			if (animTimer >= 0.85f) {
+				if(render.enabled) {
+					render.enabled = false;
+					collider.enabled = false;
+					rigidbody.isKinematic = true;
+				}
+				else if (!render.enabled) {
+					render.enabled = true;
+					collider.enabled = true;
+					rigidbody.isKinematic = false;
+					animating = true;
+				}
+				animating = false;
+				animTimer = 0;
+				animateCollider.GetComponent<Animator>().SetInteger("Open", 0);
+			}
+		}
     }
+
+
+	void OnTriggerEnter2D(Collider2D col) {
+		interacte = true;
+		animateCollider = col;
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		interacte = false;
+	}
 }
