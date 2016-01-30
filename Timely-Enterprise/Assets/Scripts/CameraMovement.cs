df65f3a108d2956812f0572b77df07934fea4f25
Kyle Des Jardins <kdesjardins@mail.bradley.edu>
2016-01-30 10:32:57 -0600

    Enemy & Backgrounds
    Adds simple Background and Enemies movement.

--- Timely-Enterprise/Assets/Scripts/CameraMovement.cs
+++ Timely-Enterprise/Assets/Scripts/CameraMovement.cs
@@ -6,14 +6,19 @@ public class CameraMovement : MonoBehaviour {
 	private Transform playerTransform;
 	[SerializeField]
 	private GameObject player;
+	private float playerY;
 
 	void awake() {
+		if (player != null) {
+			playerTransform = player.GetComponent<Transform>();
+			playerY = playerTransform.position.y + 2.71f;
+		}
 	}
 
 	void FixedUpdate () {
 		if (player != null) {
 			playerTransform = player.GetComponent<Transform>();
-			transform.position = new Vector2(playerTransform.position.x, playerTransform.position.y);
+			transform.position = new Vector2(playerTransform.position.x, playerY);
 		}
 	}
 }
