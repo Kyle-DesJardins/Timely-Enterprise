f5fa56178622ecf3a598347c0ea004108af08dcc
Kyle Des Jardins <kdesjardins@mail.bradley.edu>
2016-01-30 01:26:36 -0600

    Movable Camera
    Creates Basic character movement and the camera follows it.

---
+++ Timely-Enterprise/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	private Transform playerTransform;
+	[SerializeField]
+	private GameObject player;
+
+	void awake() {
+	}
+
+	void FixedUpdate () {
+		if (player != null) {
+			playerTransform = player.GetComponent<Transform>();
+			transform.position = new Vector2(playerTransform.position.x, playerTransform.position.y);
+		}
+	}
+}
