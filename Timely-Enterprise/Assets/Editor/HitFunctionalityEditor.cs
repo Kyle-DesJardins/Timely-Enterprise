d5698a8fa8b41375385e49fb347d27dd03928f8c
Alex Jagim <ajagim@mail.bradley.edu>
2016-01-30 02:28:59 -0600

    Interact,Hit,Kick,Duck Filler Scripts

---
+++ Timely-Enterprise/Assets/Editor/HitFunctionalityEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(HitFunctionality))]
+public class HitFunctionalityEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        HitFunctionality hitFunctionalityScript = (HitFunctionality)target;
+        hitFunctionalityScript.hittableTag = EditorGUILayout.TagField("Hittable Tag", hitFunctionalityScript.hittableTag);
+    }
+
+}
---
+++ Timely-Enterprise/Assets/Editor/InteractFunctionalityEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(InteractFunctionality))]
+public class InteractFunctionalityEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        InteractFunctionality interactFunctionalityScript = (InteractFunctionality)target;
+        interactFunctionalityScript.interactableTag = EditorGUILayout.TagField("Interactable Tag", interactFunctionalityScript.interactableTag);
+    }
+}
