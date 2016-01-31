875bde6b0f649770e082da00afd248eafa4ce248
Kyle Des Jardins <kdesjardins@mail.bradley.edu>
2016-01-30 19:36:41 -0600

    stuff

--- Timely-Enterprise/Assets/Scripts/Camera/CameraMovement.cs
+++ Timely-Enterprise/Assets/Scripts/Camera/CameraMovement.cs
@@ -7,8 +7,14 @@ public class CameraMovement : MonoBehaviour {
 	[SerializeField]
 	private GameObject player;
 	private float playerY;
+	public bool moveUp;
+	private float step;
+	[SerializeField]
+	private float speed;
 
 	void awake() {
+		moveUp = false;
+		//step = 0f;
 		if (player != null) {
 			playerTransform = player.GetComponent<Transform>();
 			playerY = playerTransform.position.y + 2.71f;
@@ -19,6 +25,16 @@ public class CameraMovement : MonoBehaviour {
 		if (player != null) {
 			playerTransform = player.GetComponent<Transform>();
 			transform.position = new Vector2(playerTransform.position.x, playerY);
+		}
+	}
+
+	void Update() {
+		if(moveUp) {
+			step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(transform.position.x, transform.position.y + 10.4f, transform.position.z), step);
+		}
+		if(playerTransform.position.y + 2.71f >= transform.position.y) {
+			moveUp = false;
 		}
 	}
 }
