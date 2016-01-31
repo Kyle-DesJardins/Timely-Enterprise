55903d33f7bf12b60b7d01b3b003c1f657350ce1
Kyle Des Jardins <kdesjardins@mail.bradley.edu>
2016-01-30 23:42:08 -0600

    audio stuff

--- Timely-Enterprise/Assets/Scripts/Audio/AudioControllers.cs
+++ Timely-Enterprise/Assets/Scripts/Audio/AudioControllers.cs
@@ -1,22 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AudioControllers : MonoBehaviour {
 
 	[SerializeField]
 	private AudioSource music;
 	[SerializeField]
-	private AudioClip audio1;
+	private AudioClip[] audio;
 	[SerializeField]
 	private Scrollbar slider;
 	private float sliderValue;
 
 	void Awake() {
-		music.clip = audio1;
+//		music.clip = audio[0];
 		slider.value = 1f;
 		slider.onValueChanged.AddListener(ControlVolume);
 		//slider = volumeLevel.GetComponent<Slider>();
+	}
+
+	void OnLevelWasLoaded(int level) {
+		music.clip = audio[SceneManager.GetActiveScene().buildIndex];
+		music.Play();
 	}
 	
 	// Update is called once per frame
