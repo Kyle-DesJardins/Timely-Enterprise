387a0f54046692841a92be09056e4561f8dbbfdc
Kyle Des Jardins <kdesjardins@mail.bradley.edu>
2016-01-31 17:00:57 -0600

    asdf

---
+++ Timely-Enterprise/Assets/Scripts/Misc/LevelCompletion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelCompletion : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.tag == "Item") {
+			if(SceneManager.GetActiveScene().buildIndex + 1 != 6) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			}
+			else {
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+}
